66c6e58d1cb97c57aa4a3d12e4c2f31c535a0508
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-12 01:03:06 +0100

    first commit

---
+++ cpp_app/README.md
@@ -0,0 +1,2 @@
+docker build . -t cpp_app:1
+docker run -it --rm --name cpp_app cpp_app:1
---
+++ cpp_app/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello running cpp code inside of a Docker container!" << std::endl;
+    return 0;
+}  
