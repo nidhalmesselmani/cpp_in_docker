267ee4e1be7a50bbf23c2c2a014e4ec7d9ab242b
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-15 22:56:49 +0100

    files

--- cpp_app/main.cpp
+++ cpp_app/main.cpp
@@ -878,23 +878,14 @@ myfile.close();
   }
   else cout << "Unable to open file";
 */
+
+/*
+
+
+
+
+*/
 int main ()
 {
-streampos size;
-  char * memblock;
 
-  ifstream file ("example.bin", ios::in|ios::binary|ios::ate);
-  if (file.is_open())
-  {
-    size = file.tellg();
-    memblock = new char [size];
-    file.seekg (0, ios::beg);
-    file.read (memblock, size);
-    file.close();
-
-    cout << "the entire file content is in memory" << memblock;
-
-    delete[] memblock;
-  }
-  else cout << "Unable to open file";
 }
